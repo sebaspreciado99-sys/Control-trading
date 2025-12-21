6ec46b3b26a44ebaad756afb1222438b44613c39
sebaspreciado99-sys <sebaspreciado99@gmail.com>
2025-12-21 16:52:54 -0500

    Update Service Worker registration path

--- app.js
+++ app.js
@@ -503,7 +503,7 @@ window.eliminarUno = eliminarUno;
 // Registro del Service Worker para PWA
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("sw.js")
+    navigator.serviceWorker.register("/Control-trading/sw.js");
       .catch(err => console.log("SW error:", err));
   });
 }
