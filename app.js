7955f3978284bcf037c0d7212fcf80f8c2d940ba
sebaspreciado99-sys <sebaspreciado99@gmail.com>
2025-12-26 23:43:48 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // URL para Google Sheets (ACTUALIZA CON TU URL)
-const URL_SHEETS = "https://script.google.com/macros/s/AKfycbxdqKDqJ46mH6v4ckctsxjwaMMLm2i4CxexQx655JjWQloGfB8-lbKVTLaxNS4a7qzMnQ/exec";
+const URL_SHEETS = "https://script.google.com/macros/s/AKfycbwhyrjxqY54qQnm11LPrzYBa7ZSFzrJLjdD2eWDhwEcPuJPLrp0CBes8r1OG_JQK81iEA/exec";
 
 let trades = JSON.parse(localStorage.getItem("trades_v5_pro")) || [];
 let sugerencias = JSON.parse(localStorage.getItem("sugerencias_v5")) || [];
